f53390b2c7f3cb9280919be10c9e25906b035a9f
Chris Hager <chris@linuxuser.at>
2017-05-07 20:08:09 +0200

    works

--- update.sh
+++ update.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 # hol die aktuelle liste an tags vom git server
+echo "Fetching latest version information..."
 git fetch --tags
 
 # was ist der neueste tag vom git server?
-git tag -l | tail -n 1
+TAG_LATEST=$( git tag -l | tail -n 1 )
 
 # was ist der tag von dem jetzigen code?
-git describe --tags
+TAG_CURRENT=$( git describe --tags )
 
+echo "current=$TAG_CURRENT, latest=$TAG_LATEST"
 # -> wenn die beiden nicht gleich sind, dann gibts ein update
+
+if [ "$TAG_CURRENT" == "$TAG_LATEST" ]; then
+    echo "Ghoust is already the latest version."
+else
+    echo "A newer version is available!"
+
+    # Update with '--update arg'
+    if [ "$1" != "--update" ]; then
+        echo "use '$0 --update' to execute the update"
+        exit 1
+    else
+        echo "update"
+    fi
+fi
+
 #git reset --hard
 #git merge tags/<tagname>
